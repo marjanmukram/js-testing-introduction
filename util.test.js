2e75c55b6d7260bd4fd84031aac678d7979b09c8
Marjan Mukram <marjanmukram8@gmail.com>
2020-07-05 16:58:42 +0530

    prettified

--- util.test.js
+++ util.test.js
@@ -6,17 +6,18 @@ test("should output name and age", () => {
 });
 
 test("should accept less args", () => {
-    const output = generateText('', 12);
-    expect(output).toBe(' (12 years old)')
-    const output2 = generateText('max', null);
-    expect(output2).toBe('max (null years old)')
-})
+  const output = generateText("", 12);
+  expect(output).toBe(" (12 years old)");
+  const output2 = generateText("max", null);
+  expect(output2).toBe("max (null years old)");
+});
 
 test("should check and generate", () => {
-    const output = checkAndGenerate('', 12);
-    expect(output).toBeFalsy();
-    const output2 = checkAndGenerate('max', null);
-    expect(output2).toBeFalsy();
-    const output3 = checkAndGenerate('max', 12);
-    expect(output3).toBe("max (12 years old)");
-})
+  const output = checkAndGenerate("", 12);
+  expect(output).toBeFalsy();
+  const output2 = checkAndGenerate("max", null);
+  expect(output2).toBeFalsy();
+  const output3 = checkAndGenerate("max", 12);
+  expect(output3).toBe("max (12 years old)");
+});
+
