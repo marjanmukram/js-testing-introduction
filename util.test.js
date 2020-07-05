ef6bb2b5f9e75df71b278e842af040231c8bd8e5
Marjan Mukram <marjanmukram8@gmail.com>
2020-07-05 17:24:35 +0530

    end to end test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 test("should output name and age", () => {
@@ -21,3 +22,21 @@ test("should check and generate", () => {
   expect(output3).toBe("max (12 years old)");
 });
 
+test("should click around", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ["--window-size=1920,1080"]
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    "file:///Users/marjan/Documents/unitTesitng/js-testing-introduction/index.html"
+  );
+  await page.click("input#name");
+  await page.type("input#name", "Mary");
+  await page.click("input#age");
+  await page.type("input#age", "28");
+  await page.click("#btnAddUser", "28");
+  const finalText = await page.$eval(".user-list", el => el.textContent);
+  expect(finalText).toBe("Mary (28 years old)");
+}, 10000);
